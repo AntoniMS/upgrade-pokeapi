7058287023d7d390ca9a1873a10aa8b1cb2cd135
Toni M <tonims.dev@gmail.com>
2021-12-18 21:46:15 +0100

    FEAT MAP / FOR

--- pokedex.js
+++ pokedex.js
@@ -3,15 +3,33 @@ const div$$ = document.querySelector(".container");
 
 const catchEmAll = async () => {
   const pokedex = [];
-  for (let i = 1; i < 152; i++) {
-    const pokedexUrl = baseUrl + [i];
 
-    const pokedexRes = await fetch(pokedexUrl);
+  for (let i = 1; i <= 151; i++) {
+    const pokedexRes = await fetch(baseUrl + i);
     const pokedexJson = await pokedexRes.json();
 
     pokedex.push(pokedexJson);
   }
-  console.log(pokedex);
+
+  const pokemon = pokedex.map((data) => ({
+    name: data.name,
+    image: data.sprites.other.home["front_default"],
+    id: data.id,
+  }));
+
+  console.log(pokemon);
 };
 
 catchEmAll();
+
+/* //ALTERNATIVA AL .MAP
+
+const pokemon = [];
+pokedex.forEach((element) => {
+    const pokemonData = {
+        name: element.name,
+        id: element.id,
+    }
+    pokemon.push(pokemonData);
+      
+  }); */
